6ee8aafa54370242ec8427198bec229395046058
engineerji <rahulkumar1491997@gmail.com>
2020-05-14 19:48:00 +0530

    revised

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddBookingComponent } from './add-booking/add-booking.component';
+import { AllBookingsComponent } from './all-bookings/all-bookings.component';
+import { FindBookingComponent } from './find-booking/find-booking.component';
+import { GetTicketComponent } from './get-ticket/get-ticket.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'addBooking',component:AddBookingComponent},
+  {path:'bookings',component:AllBookingsComponent},
+  {path:'findBooking',component:FindBookingComponent},
+  {path:'getTicket',component:GetTicketComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
